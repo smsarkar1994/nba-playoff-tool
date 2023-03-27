b5320a61ce49102c954a789eebda32380496cc46
smsarkar1994 <sameer.sarkar1994@gmail.com>
2023-03-27 15:11:25 -0400

    Comment server

--- server.R
+++ server.R
@@ -1,9 +1,12 @@
 server <- function(input, output) {
+  #Factor that scales font based on magnification specified in UI (right now is 17.5)
   font_factor = 17.5/15
   
+  #Source functions. Toggle based on filepaths
   # source(paste0("funcs.R"))
   source(paste0("/Users/smsarkar/Documents/One_offs/18_lakers_suck/lakerapp/funcs.R"))
   
+  #Scrape and clean ESPN Standings
   url = "https://www.espn.com/nba/standings"
   tmp <- read_html(url) %>%
     html_table()
@@ -15,11 +18,13 @@ server <- function(input, output) {
   dat_west <- clean_dat(dat_west, "west")
   dat <- rbind(dat_east, dat_west)
   
+  #Scrape and clean 538 predictions
   dat_538 = clean_dat538(teams)
   
+  #Execute when Run button is selected
   observeEvent(input$do, {
-    # print(input$tabs)
-    
+  
+    #Require valid team and seed selection to execute
     if(input$team %in% team_names & input$dseed <= 10) {
       if(input$ass == "proj_current") {
         output$seed_label = renderText({
@@ -34,6 +39,7 @@ server <- function(input, output) {
         ass = T
       }
 
+      #Run calculation based on selected inputs (check funcs.R for more info)
       out <- get_reqs(dat, input$team, desired_seed = input$dseed,
                       dat_538, ass_538 = ass)
       
@@ -42,6 +48,7 @@ server <- function(input, output) {
       out_dat_needed = out$data_needed
       out_text = out$text
       
+      #Print results of calculation
       output$text_output <- renderText({
         paste0("<b>", out_text, "</b>")
       })
@@ -62,13 +69,12 @@ server <- function(input, output) {
                         "</b> win pct moving forward.")
       }
       
-      
-      
-      
+    
       output$info_output <- renderText({
         paste0(label1, label2)
       })
       
+      #Standings information for Current Standings Tab
       stable <- datatable(dat %>%
                             filter(conference == out_dat_curr$conference[1]) %>%
                             mutate(record = paste0(w, "-", l),
@@ -93,7 +99,7 @@ server <- function(input, output) {
       
       output$table_standings <- DT::renderDataTable({stable})
       
-      
+      #Table with information of selected team
       currtable <- out_dat_curr %>%
         mutate(record = paste0(w, "-", l),
                record_l10 = paste0(w_last10, "-", l_last10),
@@ -258,7 +264,7 @@ server <- function(input, output) {
         select(conference, current_seed, team_full, record, pct, record_l10) %>%
         select(-conference)
       
-      
+      #Table with information of desired seed
       output$table_comparison <- renderReactable({
         reactable(
           comptable,
@@ -340,9 +346,12 @@ server <- function(input, output) {
       
     }
    
+    #Code to execute Pick Remaining Games tab
     if(input$tabs == '<strong>Pick Remaining Games</strong>' & input$team %in% team_names) {
+      #Pull and format remaining schedule
       sched <- scrape_sched(out_dat_curr, teams, dat)
 
+      #Display remaining schedule
       custom_sched <- datatable(sched %>%
                             mutate(opp_pct = format(round(opp_pct, 3), nsmall = 3)) %>%
                             select(date, opp, opp_pct, proj_win),
@@ -362,6 +371,7 @@ server <- function(input, output) {
                                           disable = list(columns = 0:2))
       ) 
 
+      #Once a cell is edited, execute
       observeEvent(input$custom_sched_cell_edit, {
         sched <<- editData(sched, 
                           input$custom_sched_cell_edit, rownames = F)
@@ -371,12 +381,15 @@ server <- function(input, output) {
           mutate(proj_win_num = ifelse(proj_win %in% c("W", "w"), 1, 
                                        ifelse(proj_win %in% c("L", "l"), 0, NA)))
         
+        #Update projected record based on edited cell
         custom_table <- get_custom_wins(currtable, out_dat_curr, sched, dat)
         
+        #Red to green scale to indicate seed
         color_seed = ifelse(custom_table$proj_seed >= 10, 
                             "#FF4E11", ifelse(custom_table$proj_seed >= 5,
                                               "#FAB733", "#69B34C"))
         
+        #Re-render projected record upon edits
         output$custom_selected <- renderReactable({
           reactable(
             custom_table,
@@ -474,7 +487,8 @@ server <- function(input, output) {
       
       output$custom_sched <- DT::renderDataTable({custom_sched})
       
-      
+      #Display projected record before edited cell (same code as above, but this initializes the table)
+      #I probably should have put this first, but don't fix what isn't broken I guess.
       custom_table <- get_custom_wins(currtable, out_dat_curr, sched, dat)
   
       print(custom_table)
